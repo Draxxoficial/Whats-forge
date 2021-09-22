85313c123a1c0bc1ec1469895fe2c68a8a1b2c64
Draxxoficial <91178873+Draxxoficial@users.noreply.github.com>
2021-09-22 03:51:03 -0300

    abrirwhats.py

--- abrirwhats.py
+++ abrirwhats.py
@@ -7,8 +7,9 @@ os.system('clear')
 new=2
 whats = str(input('Digite o NUMERO: '))
 msg = str(input('Digite a Mensagem: '))
-url = ("https://api.whatsapp.com/send?phone={}&text={}".format(whats, msg))+site+"/"
+url = ("https://api.whatsapp.com/send?phone={}&text={}".format(whats, msg))
 
-os.system('clear')
 
 webbrowser.open(url,new=new)
+
+os.system('clear')
